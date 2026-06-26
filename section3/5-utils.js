cab3cedb026cc575396cf84a057c27c8a5c6e6fa
Subham Dhar <siddy934@gmail.com>
2026-06-26 16:43:34 +0530

    section-3

---
+++ section3/5-utils.js
@@ -0,0 +1,5 @@
+const sayHi = (name) => {
+  console.log(`Hello THere ${name}`);
+};
+
+module.exports = { sayHi };
---
+++ section3/index.js
@@ -0,0 +1,55 @@
+// const amount = 12;
+
+// if (amount < 10) {
+//   console.log("small Number");
+// } else {
+//   console.log("large number");
+// }
+
+// console.log("Hey, its my first node app!!!");
+
+// ===============================================
+
+// GLOBALS = anywhere in my application , i'll access them
+
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules
+// module - info about current module
+// process - info about environment WHERE the program is being executed
+
+// ===============================================
+
+
+// console.log(__dirname) // D:\NODEJS\nodejs\section3
+
+// setInterval(()=> {
+//   console.log("hello world")
+// }, 1000)
+// ===============================================
+
+
+// const names = require("./4-names");
+// const { sayHi } = require("./5-utils")
+// const data = require("./6-alternative-flavour")
+
+// require('./7-mindgrenade')
+
+// sayHi("Susan")
+// sayHi(names.john)
+// sayHi(names.peter)
+// console.log(data)
+// console.log(data.items)
+// console.log(data.singlePerson)
+
+//every file in node is module. what we did right now is we created a module.. 
+// module = Encapsulated code(only share minimum)
+
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
